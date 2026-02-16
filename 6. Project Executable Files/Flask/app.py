7f00d5c2ed940a3f4182fad696e42e6b89f5f85f
Bhanusankar12 <119719504+Bhanusankar12@users.noreply.github.com>
2026-02-16 19:36:48 +0530

    Update app.py

--- 6. Project Executable Files/Flask/app.py
+++ 6. Project Executable Files/Flask/app.py
@@ -1,26 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route("/")
 def home():
     return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return render_template("about.html")
-
-@app.route('/dashboard')
-def dashboard():
-    return render_template("dashboard.html")
-
-@app.route('/story')
-def story():
-    return render_template("story.html")
-
-@app.route('/insights')
-def insights():
-    return render_template("insights.html")
-
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
